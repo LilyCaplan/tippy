31e2b2d4306336b7122e9aa938e1520cc096b47e
Lily Caplan <lilycaplan@Lilys-MacBook-Air.local>
2017-12-24 17:09:42 -0500

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Lily Caplan on 12/24/17.
+//  Copyright © 2017 Lily Caplan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
